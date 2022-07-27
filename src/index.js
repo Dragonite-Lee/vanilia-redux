dbe14613821b38958b80e5658ef11482c65d3590
Dragonite-Lee <ross1222@naver.com>
2022-07-27 20:32:14 +0900

    리덕스 기본연습

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { createStore } from "redux";
+
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.getElementById('number');
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+    // if (action.type === "ADD") {
+    //     return count + 1;
+    // } else if (action.type === "MINUS") {
+    //     return count - 1;
+    // } else {
+    //     return count;
+    // }
+    switch (action.type) {
+        case ADD:
+            return count + 1
+        case MINUS:
+            return count - 1 
+        default:
+            return count
+    }
+}
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({ type: ADD })
+}
+const handleMinus = () => {
+    countStore.dispatch({ type: MINUS })
+}
+add.addEventListener('click',handleAdd);
+minus.addEventListener('click',handleMinus);
